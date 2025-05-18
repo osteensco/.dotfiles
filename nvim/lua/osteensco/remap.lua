de8ca8858e62de76f66eb8aa8854bd4cf5bb36e1
Scott Osteen <osteensco@gmail.com>
2025-05-18 12:37:16 -0500

    go back to last accessed window on neotree refresh

--- nvim/lua/osteensco/remap.lua
+++ nvim/lua/osteensco/remap.lua
@@ -13,7 +13,7 @@ vim.keymap.set('n', '<leader>w', '<C-w>', { desc = "window manipulation" })
 
 --Neotree toggle
 vim.keymap.set('n', '<leader>nh', '<Cmd>Neotree toggle<CR>', { desc = 'Hide Neotree side bar' })
-vim.keymap.set('n', '<leader>nn', '<Cmd>Neotree toggle<CR><Cmd>Neotree toggle<CR>',
+vim.keymap.set('n', '<leader>nn', '<Cmd>Neotree toggle<CR><Cmd>Neotree toggle<CR><C-w>p',
     { desc = 'Refreshes Neotree side bar' })
 vim.keymap.set('n', '<leader>nb', '<Cmd>Neotree buffers float<CR>', { desc = 'Opens Neotree buffers in floating window' })
 
