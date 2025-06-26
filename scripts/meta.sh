9408b266b47e8f19ad82abc19d6fe55829189adc
Scott Osteen <osteensco@gmail.com>
2025-06-26 11:51:02 -0500

    TODO items

--- scripts/meta.sh
+++ scripts/meta.sh
@@ -51,20 +51,27 @@ fnls() {
 }
 
 
+# TODO
+#   - move these functions to a 'workflow' section
+
 # activate python virtual environment from list of available in current project
 pytivate() {
     source ~/.local/bin/pytivate -y
 }
 
-
 # start ssh server
 sshstart() {
-    sudo systemctl start sshd && systemctl status sshd
+    sudo systemctl start sshd && sudo systemctl status sshd
 }
 
 # stop ssh server
 sshstop() {
-    sudo systemctl stop sshd && systemctl status sshd
+    sudo systemctl stop sshd && sudo systemctl status sshd
+}
+
+# find and execute tmux_setup.sh
+tmuxsetup() {
+    echo "I still need to be implemented :("
 }
 
 
